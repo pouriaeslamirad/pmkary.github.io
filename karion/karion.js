78cafaa1d1c45661ef5b50ce3bdd9c8e16cdebce
Pouya Kary <k@arendelle.org>
2015-02-03 22:19:55 +0330

    time fix

--- karion/karion.js
+++ karion/karion.js
@@ -2,9 +2,11 @@ var now = new Date();
 var myBirth = new Date('01/08/1996');
 var karion = daysBetween(myBirth, now);
 var years = Math.floor( karion / 365 );
-var months = Math.floor((karion - years * 365) / 30);
-var days = Math.floor((karion - years * 365 - months * 30) - 5);
+var months = Math.floor((karion - ( years * 365 ) -Math.floor(years/4)) / 30);
+var days = karion - Math.floor(years * 365) - Math.floor(months * 30) - Math.floor(years/4) - 1;
+
 document.write(karion + "." + years + "." + months + "." + days);
+
 function daysBetween(first, second) {
     var one = new Date(first.getFullYear(), first.getMonth(), first.getDate());
     var two = new Date(second.getFullYear(), second.getMonth(), second.getDate());
