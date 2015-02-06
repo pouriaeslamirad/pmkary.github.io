a003394a556fd8b2e081fe4ba011716bb3440207
Pouya Kary <k@arendelle.org>
2015-02-06 19:22:50 +0330

    karion

--- karion/karion.js
+++ karion/karion.js
@@ -2,7 +2,7 @@ var now = new Date();
 var myBirth = new Date('01/08/1996');
 var karion = daysBetween(myBirth, now);
 var years = Math.floor( karion / 365 );
-var months = Math.floor((karion - ( years * 365 ) -Math.floor(years/4)) / 30);
+var months = Math.floor((karion - ( years * 365 ) -Math.floor((years+1)/4)) / 30);
 var days = karion - Math.floor(years * 365) - Math.floor(months * 30) - Math.floor(years/4) - 1;
 
 document.write(karion + "." + years + "." + months + "." + days);
